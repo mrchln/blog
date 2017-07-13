abd3aa86c67e5b663b8276d1efc3b19bd7ff0ae6
flchy <248451597@qq.com>
2017-07-13 21:47:48 +0800

    修改
    Signed-off-by: flchy <248451597@qq.com>

--- flchy-blog-parent/flchy-blog-inlets/src/main/java/com/flchy/blog/Application.java
+++ flchy-blog-parent/flchy-blog-inlets/src/main/java/com/flchy/blog/Application.java
@@ -1,8 +1,8 @@
 package com.flchy.blog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
---
+++ flchy-blog-parent/flchy-blog-privilege/src/main/java/org/flchy/blog/privilege/App.java
@@ -0,0 +1,13 @@
+package org.flchy.blog.privilege;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
