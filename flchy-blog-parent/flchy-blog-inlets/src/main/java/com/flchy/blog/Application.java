c0d4c9ecf0a0a0c5d30c588217f582c0d9e0ab08
flchy <248451597@qq.com>
2017-07-15 01:43:55 +0800

    1

--- flchy-blog-parent/flchy-blog-inlets/src/main/java/com/flchy/blog/Application.java
+++ flchy-blog-parent/flchy-blog-inlets/src/main/java/com/flchy/blog/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(Application.class);
 		// springApplication.addListeners(new ApplicationStartupListener());
